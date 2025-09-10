b67ab9af35f522c37a769833c3857023f41e2f6c
MorsyB <morsyb.6c@gmail.com>
2025-09-10 11:49:14 +0300

    `Update origin in CORS configuration to remove unnecessary quotes`

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import { createApartment } from "./apartments/post";
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', '"https://apartments-rental-1.onrender.com'],
+  origin: ['http://localhost:3000', 'https://apartments-rental-1.onrender.com'],
   credentials: true
 }));
 app.use(express.json());
